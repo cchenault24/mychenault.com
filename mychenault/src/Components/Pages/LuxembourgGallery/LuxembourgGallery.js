b0ec16b9c69a2cb457b5992e1f153b2bff3ce4f5
Christian Chenault <45714622+cchenault24@users.noreply.github.com>
2019-02-22 21:59:32 -0500

    Added Lightbox component

--- mychenault/src/Components/Pages/LuxembourgGallery/LuxembourgGallery.js
+++ mychenault/src/Components/Pages/LuxembourgGallery/LuxembourgGallery.js
@@ -5,6 +5,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { withStyles } from "@material-ui/core/styles";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import Gallery from "react-photo-gallery";
+import Lightbox from "react-images";
 import theme from "theme";
 import { Typography } from "@material-ui/core";
 
@@ -78,6 +79,38 @@ const photos = [
 ];
 
 class LuxembourgGallery extends React.Component {
+    constructor() {
+        super();
+        this.state = { currentImage: 0 };
+        this.closeLightbox = this.closeLightbox.bind(this);
+        this.openLightbox = this.openLightbox.bind(this);
+        this.gotoNext = this.gotoNext.bind(this);
+        this.gotoPrevious = this.gotoPrevious.bind(this);
+    }
+
+    openLightbox(event, obj) {
+        this.setState({
+            currentImage: obj.index,
+            lightboxIsOpen: true,
+        });
+    }
+    closeLightbox() {
+        this.setState({
+            currentImage: 0,
+            lightboxIsOpen: false,
+        });
+    }
+    gotoPrevious() {
+        this.setState({
+            currentImage: this.state.currentImage - 1,
+        });
+    }
+    gotoNext() {
+        this.setState({
+            currentImage: this.state.currentImage + 1,
+        });
+    }
+
     render() {
         const { classes } = this.props;
 
@@ -95,7 +128,15 @@ class LuxembourgGallery extends React.Component {
                         >
                             Luxembourg
                         </Typography>
-                        <Gallery photos={photos} />
+                        <Gallery photos={photos} onClick={this.openLightbox} />
+                        <Lightbox
+                            images={photos}
+                            onClose={this.closeLightbox}
+                            onClickPrev={this.gotoPrevious}
+                            onClickNext={this.gotoNext}
+                            currentImage={this.state.currentImage}
+                            isOpen={this.state.lightboxIsOpen}
+                        />
                     </div>
                 </React.Fragment>
             </MuiThemeProvider>
